17b52444d08da10634078232d11b82491c5617b8
Hiren <ghg64272@gmail.com>
2025-07-22 18:40:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,24 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    },
+    server: {
+      port: Number(process.env.PORT) || 4000,
+      host: '0.0.0.0'
+    },
+    preview: {
+      allowedHosts: ['a4f-voice.onrender.com']  // <== this is key
+    }
+  };
 });
